56d994d573eb96326a0c7ca33dec467ed48d5083
tem3 <taylormoore3@gmail.com>
2018-12-20 00:24:44 -0500

    store 3 word series stem

--- prawbot.py
+++ prawbot.py
@@ -1,6 +1,6 @@
 import praw
 import sqlite3
-
+import time 
 from prawbot_creds import client_id, client_secret, password, user_agent, username
 
 
@@ -11,8 +11,8 @@ reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
 
 
 
-def open_db_connection():
-    conn = sqlite3.connect('test.db')
+def open_db_connection(db):
+    conn = sqlite3.connect(db)
     c = conn.cursor()
     return conn, c
 
@@ -29,12 +29,13 @@ def get_author_info(author):
     return author_info
     
 def add_new_stories():
-    conn, c = open_db_connection()
+    conn, c = open_db_connection('test.db')
     subreddit = reddit.subreddit('nosleep')
     nosleep_stream = subreddit.stream.submissions()
     
     for submission in nosleep_stream:
         if not submission.stickied:
+            print(submission.link_flair_css_class)
             print(submission.link_flair_text)
             submission_info = get_submission_info(submission)
             author_info = get_author_info(submission.author)
@@ -43,18 +44,44 @@ def add_new_stories():
                       VALUES (?,?,?,?)', submission_info)
             c.execute('INSERT OR IGNORE INTO authors(author_id, author_name)\
                         VALUES (?,?)', author_info)
+            insert_series(c, submission, author)
             conn.commit()
    
     close_db_connection(conn)
-##            print('Date: {}, Title: {}, author name: {},\
-##                  author id: {}, submission_id: {}'.format(submission.created_utc,
-##                                                                                        submission.title,
-##                                                                                       submission.author,
-##                                                                                       submission.author.id,
-##                                                                                       submission.id))
+                               
+def insert_series(c, submission, author):
+    
+    c.execute('SELECT * FROM series WHERE author_id = ? ORDER BY title_stem', (author.id,))
+    stored_stems = [a[1] for a in c.fetchall()]
+    title = submission.title
+    title_stem_list = title.split()[0:3]
+    print(title_stem_list)
+    title_stem = " ".join(title_stem_list)
+    print(title_stem)
+    if title_stem not in stored_stems:
+        c.execute('INSERT OR IGNORE INTO series (title_stem, author_id) VALUES (?,?)', (title_stem, author.id))
 
-
-
+    
+def add_hot_stories():
+    conn, c = open_db_connection('hot.db')
+    subreddit = reddit.subreddit('nosleep')
+    while True:
+        nosleep_stream = subreddit.hot(limit=10)
+        for submission in nosleep_stream:
+            if not submission.stickied:
+                flair_type = submission.link_flair_css_class
+                submission_info = get_submission_info(submission)
+                author_info = get_author_info(submission.author)
+                print(submission_info, author_info)
+                c.execute('INSERT OR IGNORE INTO stories(story_id, story_title, author_id, created)\
+                          VALUES (?,?,?,?)', submission_info)
+                c.execute('INSERT OR IGNORE INTO authors(author_id, author_name)\
+                            VALUES (?,?)', author_info)
+                if flair_type == 'flair-series':
+                    insert_series(c, submission, submission.author)
+                conn.commit()
+        time.sleep(16)
+    close_db_connection(conn)
 
 if __name__ == '__main__':
-    add_new_stories()
+    add_hot_stories()
